5d25798714ade41f4ab4b7aaca0c39018730c9b0
Russell Gordon <rgordon@lcs.on.ca>
2024-10-03 06:30:50 -0400

    Initial Commit

---
+++ LayoutExamples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LayoutExamples
+//
+//  Created by Russell Gordon on 2024-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LayoutExamples/LayoutExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExamplesApp.swift
+//  LayoutExamples
+//
+//  Created by Russell Gordon on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
